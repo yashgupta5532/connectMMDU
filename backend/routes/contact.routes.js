8ffdb64d32f6c9d04a72be117fef50b49ceff005
YASH GUPTA <yashgupta5532@gmail.com>
2024-01-26 17:48:05 +0530

    added c for C contact controller in routes

--- backend/routes/contact.routes.js
+++ backend/routes/contact.routes.js
@@ -1,10 +1,15 @@
-import express from "express"
-import { createContact, getAllContact } from "../controller/ContactController.js"
-import { isAdmin, verifyJwt } from "../middleware/auth.middleware.js"
+import express from "express";
+import {
+  createContact,
+  getAllContact,
+} from "../controller/contactController.js";
+import { isAdmin, verifyJwt } from "../middleware/auth.middleware.js";
 
-const router=express.Router();
+const router = express.Router();
 
-router.route("/create-contact").post(createContact)
-router.route("/admin/all-contact").get(verifyJwt,isAdmin, isAdmin,getAllContact)  //admin need to change later
+router.route("/create-contact").post(createContact);
+router
+  .route("/admin/all-contact")
+  .get(verifyJwt, isAdmin, isAdmin, getAllContact); //admin need to change later
 
-export default router
+export default router;

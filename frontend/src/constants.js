dff99434ccc5189e73835b6e6b749075cc2bcbea
YASH GUPTA <yashgupta5532@gmail.com>
2024-01-27 10:35:08 +0530

    changes mongdb atlas url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,8 @@
-export const serverUrl="http://localhost:4000/api/v1"
+// export const serverUrl="http://localhost:4000/api/v1"
 
 
-// export const serverUrl="https://connectmmdu-backend.onrender.com"
+export const serverUrl="https://connectmmdu-backend.onrender.com"
 
-// export const socketUrl="https://connectmmdu-backend.onrender.com"
+export const socketUrl="https://connectmmdu-backend.onrender.com"
 
-export const socketUrl="http://localhost:4000"
+// export const socketUrl="http://localhost:4000"
